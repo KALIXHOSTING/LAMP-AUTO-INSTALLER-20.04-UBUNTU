283f8c56f965aee12f8ae0eb0137986b8495505b
KALIXHOSTING <93708739+KALIXHOSTING@users.noreply.github.com>
2023-02-02 17:22:25 -0600

    LAMP STACK AUTOINSTALL
    APACHE 
    PHP 7.4
    COMPOSER
    FFMPEG
    MARIADB
    PHPMYADMIN

--- install.sh
+++ install.sh
@@ -186,6 +186,13 @@ sudo apt-get install php7.4-bcmath -y
 
 echo 'done'
 
+echo 'PHP 7.4 more mods'
+
+sudo apt install php7.4 libapache2-mod-php7.4 php7.4-common php7.4-sqlite3 php7.4-curl php7.4-fpm php7.4-dev php7.4-curl php7.4-xmlrpc php7.4-gd php7.4-mysql php7.4-mbstring php7.4-xml php7.4-soap php7.4-cli php7.4-zip php7.4-bz2 php7.4-dba php7.4-imap php7.4-intl php7.4-ldap php7.4-cli php7.4-cgi php7.4-fpm -y
+
+echo 'done'
+
+
 echo 'rar SET UP'
 
 sudo apt-get install rar -y
